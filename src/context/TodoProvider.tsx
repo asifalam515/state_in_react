58a34d60dcfebbe4b620f41d1a87ce749594d592
asifalam515 <asibulalam515@gmail.com>
2025-01-13 10:31:15 +0600

    type added

--- src/context/TodoProvider.tsx
+++ src/context/TodoProvider.tsx
@@ -1,12 +1,35 @@
-import { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 
-export const TodoContext = createContext(undefined);
+export const TodoContext = createContext<
+  { state: TTodo[]; dispatch: React.Dispatch<TAction> } | undefined
+>(undefined);
 type TodoProviderProps = {
   children: React.ReactNode;
 };
+type TTodo = {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+};
+type TAction = {
+  type: string;
+  payload: TTodo;
+};
+const initialState: TTodo[] = [];
+const reducer = (currentState: TTodo, action: TAction) => {
+  switch (action.type) {
+    case "addTodo":
+      return [...currentState, action.payload];
+
+    default:
+      return currentState;
+  }
+};
 const TodoProvider = ({ children }: TodoProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const values = {
-    todoTitle: "this is todo title",
+    state,
+    dispatch,
   };
   return <TodoContext.Provider value={values}>{children}</TodoContext.Provider>;
 };
